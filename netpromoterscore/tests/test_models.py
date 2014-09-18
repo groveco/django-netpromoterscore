b0ab787de46f5b0c7486bc8921963521fca21d59
Igor Stroganov <ihar.strohanau@itechart-group.com>
2014-09-18 17:48:35 +0300

    Fix models tests

--- netpromoterscore/tests/test_models.py
+++ netpromoterscore/tests/test_models.py
@@ -1,4 +1,5 @@
 import datetime
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.test import TestCase
 from netpromoterscore.models import PromoterScore, NetPromoterScore
@@ -6,19 +7,26 @@ from netpromoterscore.models import PromoterScore, NetPromoterScore
 
 class TestPromoterScoreModels(TestCase):
     def setUp(self):
-        self.user1 = User.objects.create_user(username='jared', email='jared@hotmail.com', password='foobar123')
-        self.user1.save()
-        self.user2 = User.objects.create_user(username='cole', email='cole@hotmail.com', password='foobar321')
+        self.user_model = get_user_model()
+
+        self.user1 = self.user_model.objects.create_user(
+            username='jared', email='jared@hotmail.com', password='foobar123'
+        )
+        self.user2 = self.user_model.objects.create_user(
+            username='cole', email='cole@hotmail.com', password='foobar321'
+        )
+
         self.now = datetime.datetime.now()
+
         self.ps1 = PromoterScore(user=self.user1, score=8)
         self.ps1.save()
         self.ps2 = PromoterScore(user=self.user1, score=9)
         self.ps2.save()
-        self.ps2.created_at = self.now+datetime.timedelta(-4*365/12)
+        self.ps2.created_at = self.now - datetime.timedelta(4*365/12)
         self.ps2.save()
         self.ps3 = PromoterScore(user=self.user2, score=9)
         self.ps3.save()
-        self.ps3.created_at = self.now+datetime.timedelta(-7*365/12)
+        self.ps3.created_at = self.now - datetime.timedelta(7*365/12)
         self.ps3.save()
 
     def test_rolling_months(self):
