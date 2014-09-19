8d69a2f5f1ea88d09c38a2954ccd2fb8442d3af9
Igor Stroganov <ihar.strohanau@itechart-group.com>
2014-09-19 14:27:45 +0300

    Refactoring

--- netpromoterscore/models.py
+++ netpromoterscore/models.py
@@ -1,73 +1,26 @@
+import datetime
 from django.db import models
 from django.conf import settings
-from utils import monthDict, get_many_previous_months, get_next_month
-import datetime
+from utils import get_next_month
 
 
 class PromoterScoreManager(models.Manager):
-
-    def get_list_view_context(self, rolling):
-        now = datetime.date.today().replace(day=1)
-
-        months = [now] + get_many_previous_months(now)
-        scores_by_month = [self._get_netpromoter(month, rolling) for month in months]
-        return scores_by_month
-
-    def promoters(self, scores):
-        return self.segment(scores, lambda x: 9 <= x <= 10)
-
-    def detractors(self, scores):
-        return self.segment(scores, lambda x: 1 <= x <= 6)
-
-    def passive(self, scores):
-        return self.segment(scores, lambda x: 7 <= x <= 8)
-
-    def skipped(self, scores):
-        return self.segment(scores, lambda x: x is None)
-
-    def segment(self, scores, test):
-        return sum([test(score) for score in scores.values()])
-
-    def _rolling(self, month):
+    def rolling(self, month):
         month = get_next_month(month)
-        scores = self.filter(created_at__lt=datetime.date(year=month.year, month=month.month, day=1)).order_by('-created_at').values('user', 'score')
-        return self.recent_scores(scores)
+        scores = self.filter(created_at__lt=datetime.date(year=month.year, month=month.month, day=1))\
+            .order_by('-created_at').values('user', 'score')
+        return self._recent_scores(scores)
 
-    def _one_month_only(self, month):
+    def one_month_only(self, month):
         scores = self.filter(created_at__month=month.month, created_at__year=month.year).values('user', 'score')
-        return self.recent_scores(scores)
+        return self._recent_scores(scores)
 
-    def recent_scores(self, scores):
+    def _recent_scores(self, scores):
         most_recent_scores = {}
         for score in scores:
             if not score['user'] in most_recent_scores:
                 most_recent_scores[score['user']] = score['score']
         return most_recent_scores
-
-    def _get_netpromoter(self, month, rolling):
-        label = monthDict[month.month] + ' ' + str(month.year)
-        scores = self._rolling(month) if rolling else self._one_month_only(month)
-        return NetPromoterScore(label, self.promoters(scores), self.detractors(scores), self.passive(scores), self.skipped(scores))
-
-
-class NetPromoterScore(object):
-
-    def __init__(self, label, promoters, detractors, passive, skipped):
-        self.label = label
-        self.promoters = promoters
-        self.detractors = detractors
-        self.passive = passive
-        self.skipped = skipped
-
-    @property
-    def score(self):
-        total = self.promoters + self.detractors + self.passive
-        if total > 0:
-            promoter_percentage = float(self.promoters) / float(total)
-            detractor_percentage = float(self.detractors) / float(total)
-            return round((promoter_percentage - detractor_percentage) * 100, 2)
-        else:
-            return 'Not enough information.'
 
 
 class PromoterScore(models.Model):
--- netpromoterscore/views.py
+++ netpromoterscore/views.py
@@ -7,25 +7,34 @@ from django.views.generic.base import View
 from .models import PromoterScore
 from .forms import PromoterScoreForm
 from .decorators import login_required, admin_required
+from .utils import get_many_previous_months, monthDict, count_score
+
+
+CHECKS = {
+    'promoters': lambda x: 9 <= x <= 10,
+    'detractors': lambda x: 1 <= x <= 6,
+    'passive': lambda x: 7 <= x <= 8,
+    'skipped': lambda x: x is None
+}
 
 
 class SurveyView(View):
 
     @method_decorator(login_required)
     def get(self, request):
-        data = {'survey_is_needed': True if self._user_needs_survey(request.user) else False}
+        data = {'survey_is_needed': True if self.user_needs_survey(request.user) else False}
         return HttpResponse(json.dumps(data), content_type="application/json")
 
     @method_decorator(login_required)
     def post(self, request):
-        promoter_score, errors = self._get_promoter_score(request)
+        promoter_score, errors = self.get_promoter_score(request)
         if promoter_score:
             data, status = {'id': promoter_score.pk}, 200
         else:
             data, status = errors, 400
         return HttpResponse(json.dumps(data), content_type='application/json', status=status)
 
-    def _get_promoter_score(self, request):
+    def get_promoter_score(self, request):
         data = json.loads(request.body)
         data['user'] = request.user.id
         pk = data.pop('id', None)
@@ -39,16 +48,16 @@ class SurveyView(View):
 
         return form.save() if form.is_valid() else None, form.errors
 
-    def _user_needs_survey(self, user):
-        promoter_score = self._get_most_recent_promoter_score(user)
+    def user_needs_survey(self, user):
+        promoter_score = self.get_most_recent_promoter_score(user)
         if promoter_score:
-            print promoter_score.created_at, self._time_to_ask(promoter_score), datetime.datetime.now()
-        return not promoter_score or self._time_to_ask(promoter_score) < datetime.datetime.now()
+            print promoter_score.created_at, self.time_to_ask(promoter_score), datetime.datetime.now()
+        return not promoter_score or self.time_to_ask(promoter_score) < datetime.datetime.now()
 
-    def _get_most_recent_promoter_score(self, user):
+    def get_most_recent_promoter_score(self, user):
         return PromoterScore.objects.filter(user=user).order_by('-created_at').first()
 
-    def _time_to_ask(self, promoter_score):
+    def time_to_ask(self, promoter_score):
         return promoter_score.created_at.replace(tzinfo=None) + datetime.timedelta(6*365/12)
 
 
@@ -56,6 +65,34 @@ class NetPromoterScoreView(View):
 
     @method_decorator(admin_required)
     def get(self, request):
-        rolling = True if request.GET.get('rolling') and int(request.GET.get('rolling')) else False
-        context = {'rolling': rolling, 'nps_info_list': PromoterScore.objects.get_list_view_context(rolling)}
+        context = self.get_context_data(request)
         return render(request, 'netpromoterscore/base.html', context)
+
+    def get_context_data(self, request):
+        rolling = True if request.GET.get('rolling') and int(request.GET.get('rolling')) else False
+        return {'rolling': rolling, 'nps_info_list': self.get_list_view_context(rolling)}
+
+    def get_list_view_context(self, rolling):
+        now = datetime.date.today().replace(day=1)
+
+        months = [now] + get_many_previous_months(now)
+        scores_by_month = [self.get_netpromoter(month, rolling) for month in months]
+        return scores_by_month
+
+    def get_netpromoter(self, month, rolling):
+        label = monthDict[month.month] + ' ' + str(month.year)
+        if rolling:
+            scores = PromoterScore.objects.rolling(month)
+        else:
+            scores = PromoterScore.objects.one_month_only(month)
+
+        result = {}
+        for name, check in CHECKS.items():
+            result[name] = self.sum(scores, check)
+        result['score'] = count_score(**result)
+        result['label'] = label
+
+        return result
+
+    def sum(self, scores, check):
+        return sum([1 for score in scores.values() if check(score)])
