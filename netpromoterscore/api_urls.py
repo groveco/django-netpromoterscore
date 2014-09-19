b89824194c5227869e06f1d1e70b3c598e03075d
Igor Stroganov <ihar.strohanau@itechart-group.com>
2014-09-19 15:15:39 +0300

    Delete urls

--- netpromoterscore/api_urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from .views import SurveyView
-
-urlpatterns = patterns('',
-    url(r'survey/$', SurveyView.as_view(), name='survey'),
-)
--- netpromoterscore/app_urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from .views import NetPromoterScoreView
-
-urlpatterns = patterns('',
-    url(r'$', NetPromoterScoreView.as_view(), name='net-promoter-score'),
-)
