d7206bf38677b5787c0613449312bf96d2da73ac
Anton Shutik <anton.shutik@itechart-group.com>
2014-11-24 13:53:33 +0300

    Merge

--- netpromoterscore/models.py
+++ netpromoterscore/models.py
@@ -1,4 +1,3 @@
-import datetime
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.conf import settings
@@ -18,8 +17,8 @@ class PromoterScoreManager(models.Manager):
 
 class PromoterScore(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    score = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
-    reason = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    score = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(10)])
+    reason = models.TextField(null=True, blank=True)
 
     objects = PromoterScoreManager()
